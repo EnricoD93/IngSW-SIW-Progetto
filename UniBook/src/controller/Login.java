32c97a83b8a9284991e55d0875e568fb7458ff17
martina95 <32544742+martina95@users.noreply.github.com>
2017-12-27 18:07:44 +0100

    login test failed :)

--- UniBook/src/controller/Login.java
+++ UniBook/src/controller/Login.java
@@ -8,11 +8,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 public class Login extends HttpServlet {
-@Override
-protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	HttpSession session =req.getSession();
-	session.setAttribute("matricola", "");
-		String username = (String) session.getAttribute("matricola");	
-}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("ciao");
+		HttpSession session = req.getSession();
+		session.setAttribute("matricola", "");
+		 String username = (String) session.getAttribute("matricola");
+
+		// NON FUNZIONA
+
+		// StudenteDao studenteDao =
+		// DatabaseManager.getInstance().getDaoFactory().getStudenteDAO();
+		// if (username.equals(studenteDao.findByPrimaryKey(username).getMatricola())) {
+		// System.out.println("esiste");
+		// } else
+		// System.out.println("non esiste");
+	}
 }
