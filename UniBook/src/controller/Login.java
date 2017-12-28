157056bddfa742ecb1c5626c5966ffa8ac0222fd
martina95 <32544742+martina95@users.noreply.github.com>
2017-12-28 13:55:07 +0100

    login servlet fixed

--- UniBook/src/controller/Login.java
+++ UniBook/src/controller/Login.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,9 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.Studente;
 import persistence.DatabaseManager;
 import persistence.dao.StudenteDao;
-
 
 public class Login extends HttpServlet {
 	@Override
@@ -18,15 +19,16 @@ public class Login extends HttpServlet {
 		System.out.println("ciao");
 		HttpSession session = req.getSession();
 		session.setAttribute("matricola", "");
-		 String username = (String) session.getAttribute("matricola");
+		String username =  req.getParameter("matricola");
+		System.out.println("username:" + username);
 
 		// NON FUNZIONA
-
-		 StudenteDao studenteDao = DatabaseManager.getInstance().getDaoFactory().getStudenteDAO();
-		 if (username.equals(studenteDao.findByPrimaryKey(username).getMatricola())) {
-		 System.out.println("esiste");
-		 } else
-		 System.out.println("non esiste");
+		StudenteDao studenteDao = DatabaseManager.getInstance().getDaoFactory().getStudenteDAO();	
+		System.out.println(studenteDao.findByPrimaryKey(username).getMatricola());
+		if (username.equals(studenteDao.findByPrimaryKey(username).getMatricola())) {
+			System.out.println("esiste");
+		} else
+			System.out.println("non esiste");
 	}
-	
+
 }
